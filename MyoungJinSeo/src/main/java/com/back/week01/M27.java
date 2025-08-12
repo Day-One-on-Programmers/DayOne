703d28b8ebb99dd655b25f5ddd6648fa336e6508
myoungjinseo <sih5673@gmail.com>
2025-08-12 17:25:10 +0900

    [DOOP-10] feat: 향상된 for문

---
+++ MyoungJinSeo/src/main/java/com/back/week01/M27.java
@@ -0,0 +1,13 @@
+package com.back.week01;
+
+public class M27 {
+    /**
+     향상된 for문을 사용해서 1차원 배열 array 원소를 한 줄씩 출력하세요.
+     */
+    public static void main(String[] args) {
+        int [] array = {2, 5, 8, 10, 22, 55, 100};
+        for(int value : array){
+            System.out.println(value);
+        }
+    }
+}
