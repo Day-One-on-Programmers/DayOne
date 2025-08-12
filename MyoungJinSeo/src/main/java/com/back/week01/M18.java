ee00573b88ec220216a045435655cebfb8d9f26d
myoungjinseo <sih5673@gmail.com>
2025-08-12 16:34:53 +0900

    [DOOP-10] feat: 메소드 선언

---
+++ MyoungJinSeo/src/main/java/com/back/week01/M18.java
@@ -0,0 +1,32 @@
+package com.back.week01;
+
+public class M18 {
+    /**
+     * 1. 매개변수가 있고 결과값을 반환하는 메소드를 선언하세요.
+     * - 메소드명 : methodA
+     * - 매개변수 : 정수 값을 받고, 변수명은 a
+     * - 결과값 반환 : boolean타입 값 true를 반환
+     */
+    public boolean methodA(int a) {
+        return true;
+    }
+
+    /**
+     * 2. 매개변수가 있는 메소드를 선언하세요. (결과값은 반환하지 않습니다)
+     * - 메소드명 : methodB
+     * - 매개변수 : 정수 값을 받고, 변수명은 a
+     */
+    public void methodB(int a) {}
+    /**
+     * 3. 매개변수가 없는 메소드를 선언하세요. (결과값은 반환하지 않습니다)
+     * - 메소드명 : methodC
+     */
+    public void methodC() {}
+
+    public static void main(String[] args) {
+        M18 practice = new M18();
+        System.out.println(practice.methodA(10));
+        practice.methodB(10);
+        practice.methodC();
+    }
+}
