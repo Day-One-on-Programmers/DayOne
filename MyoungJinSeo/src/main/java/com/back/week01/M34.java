ecb1acf4ba10b50864c497ad1a7471b4a1475c6a
myoungjinseo <sih5673@gmail.com>
2025-08-12 20:42:15 +0900

    [DOOP-10] feat: static final

---
+++ MyoungJinSeo/src/main/java/com/back/week01/M34.java
@@ -0,0 +1,16 @@
+package com.back.week01;
+
+public class M34 {
+    /**
+     * 클래스 상수를 선언하세요. (static final 활용)
+     * - 필드명은 MAX_RPM
+     * - 정수 타입으로 선언하고 값은 300으로 초기화합니다.
+     */
+    static final int MAX_RPM = 300;
+
+
+    // 아래는 결과 평가를 위한 코드입니다. 수정하지 마세요.
+    public static void main(String[] args) {
+        System.out.println(M34.MAX_RPM);
+    }
+}
