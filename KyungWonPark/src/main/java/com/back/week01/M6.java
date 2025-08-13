3205b90bb868f4b0ff3bdcbf00d159a8314fd8d9
경원 <kyungwon.park0407@gmail.com>
2025-08-14 03:08:55 +0900

    기본 타입 변환-2

---
+++ KyungWonPark/src/main/java/com/back/week01/M6.java
@@ -0,0 +1,18 @@
+package com.back.week01;
+
+//문제 설명
+//float 타입 변수 floatValue의 값을 int 타입 변수로 넣으려 합니다.
+//빈칸에 int타입 변수 intValue를 선언하고 floatValue를 이용해 초기화해보세요.
+
+
+public class M6 {
+    public static void main(String[] args) {
+        float floatValue = 2.5f;
+        // int타입 변수 intValue를 선언하고 floatValue를 이용해 초기화해보세요.
+
+        int intValue = (int)floatValue;
+
+
+        System.out.println(intValue);
+    }
+}
