b0bcf157e916e8f962451649dfd0ecfd1b89b0f2
myoungjinseo <sih5673@gmail.com>
2025-08-13 18:41:19 +0900

    [DOOP-18] feat : 자연수 뒤집어 배열로 만들기

---
+++ MyoungJinSeo/src/main/java/com/back/week02/M1.java
@@ -0,0 +1,19 @@
+package com.back.week02;
+
+import java.util.Arrays;
+
+public class M1 {
+    public static void main(String[] args) {
+        int n = 12345;
+        System.out.println(Arrays.toString(solution(n)));
+    }
+    public static int[] solution(long n) {
+        int length = String.valueOf(n).length();
+        int[] answer = new int[length];
+        for (int i = 0; i < length; i++) {
+            answer[i] = (int) (n % 10);
+            n /= 10;
+        }
+        return answer;
+    }
+}
