7fdb46c2fccba907e7b0d098c2f425ceea2d9d7f
myoungjinseo <sih5673@gmail.com>
2025-08-18 16:29:15 +0900

    [DOOP-18] feat : 최대 값 인덱스 구하기

---
+++ MyoungJinSeo/src/main/java/com/back/week02/M2.java
@@ -0,0 +1,17 @@
+package com.back.week02;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class M2 {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,4,5,2,1};
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+    public static int[] solution(int[] arr) {
+        int max = Arrays.stream(arr).max().getAsInt();
+        return IntStream.range(0, arr.length)
+                .filter(i -> arr[i] == max)
+                .toArray();
+    }
+}
