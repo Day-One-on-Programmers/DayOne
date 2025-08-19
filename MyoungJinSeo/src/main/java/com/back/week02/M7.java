978b067d9d1b0c09657b10f3692d4f372b8d24e3
myoungjinseo <sih5673@gmail.com>
2025-08-19 16:27:37 +0900

    [DOOP-18] feat : 같은 숫자는 싫어

---
+++ MyoungJinSeo/src/main/java/com/back/week02/M7.java
@@ -0,0 +1,22 @@
+package com.back.week02;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class M7 {
+    public static void main(String[] args) {
+        int[] arr = {1,1,3,3,0,1,1};
+        System.out.println(Arrays.toString(solution(arr)));
+    }
+    public static int[] solution(int []arr) {
+        List<Integer> result = new ArrayList<>();
+        result.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i] != arr[i-1]){
+                result.add(arr[i]);
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
