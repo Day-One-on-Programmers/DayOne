c55d32a6d96746795432946bd1b70218e9e7491a
경원 <kyungwon.park0407@gmail.com>
2025-08-19 17:35:26 +0900

    [DOOP-17] feat: 삼항연산자

---
+++ KyungWonPark/src/main/java/com/back/week01/M10.java
@@ -0,0 +1,13 @@
+package com.back.week01;
+
+public class M10 {
+    public static void main(String[] args) {
+        int score = 88;
+
+        // score 점수가 90보다 크거나 같으면 'A'를, 90보다 작으면 'B' 결과를 반환하도록 작성하세요.
+        char grade =
+                score >= 90 ? 'A' : 'B';
+        ;
+        System.out.println(grade == 'B');
+    }
+}
