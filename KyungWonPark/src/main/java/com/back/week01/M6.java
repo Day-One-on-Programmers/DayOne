d9955986adbc6372656eaa46b473390ec5958686
경원 <kyungwon.park0407@gmail.com>
2025-08-19 17:20:25 +0900

    [DOOP-17] feat:ë 단í단항ì연산자

--- KyungWonPark/src/main/java/com/back/week01/M6.java
+++ KyungWonPark/src/main/java/com/back/week01/M6.java
@@ -7,12 +7,18 @@ package com.back.week01;
 
 public class M6 {
     public static void main(String[] args) {
-        float floatValue = 2.5f;
-        // int타입 변수 intValue를 선언하고 floatValue를 이용해 초기화해보세요.
-
-        int intValue = (int)floatValue;
-
-
-        System.out.println(intValue);
+        int x = 10;
+        // x의 음수 연산
+        x = -x;
+        // x의 양수 연산
+        x = +x;
+        // x의 증가 연산
+        x++;
+        // x의 감소 연산
+        x--;
+        boolean isTrue = x == 11;
+        // isTrue의 논리 부정 연산
+        isTrue = !isTrue;
+        System.out.println(isTrue);
     }
 }
---
+++ KyungWonPark/src/main/java/com/back/week01/M7.java
@@ -0,0 +1,11 @@
+package com.back.week01;
+
+//빈칸 채우기는 이미 완성된 코드 중 빈칸에 알맞은 코드를 입력하는 문제 타입입니다.
+//빈칸을 제외한 기본 코드는 수정할 수 없습니다.
+//빈칸을 채우지 않을 경우, 실행 결과에 에러 메시지가 표시됩니다.
+
+public class M7 {
+    public static void main(String[] args) {
+
+    }
+}
