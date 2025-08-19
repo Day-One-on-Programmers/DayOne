38b0857192baaab1db01d6587f6074f3d9018796
myoungjinseo <sih5673@gmail.com>
2025-08-19 16:41:18 +0900

    [DOOP-18] feat : 끝말 잇기

---
+++ MyoungJinSeo/src/main/java/com/back/week02/M9.java
@@ -0,0 +1,26 @@
+package com.back.week02;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class M9 {
+    public static void main(String[] args) {
+        String[] strArr = {"tank","kick","wheel","land","dream","mother","robot","tank"};
+        System.out.println(solution(strArr));;
+    }
+
+    public static boolean solution(String[] arr) {
+        Set<String> set = Arrays.stream(arr).collect(Collectors.toSet());
+        if(set.size() != arr.length)
+            return false;
+        char last = arr[0].charAt(arr[0].length() - 1);
+        for (int i = 1; i < arr.length; i++) {
+            char first = arr[i].charAt(0);
+            if(last != first)
+                return false;
+            last = arr[i].charAt(arr[i].length() - 1);
+        }
+        return true;
+    }
+}
