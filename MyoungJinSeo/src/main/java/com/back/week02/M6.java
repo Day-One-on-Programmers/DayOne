1bc47aeeb43cd549442071f07b8e23c97a4d0223
myoungjinseo <sih5673@gmail.com>
2025-08-19 16:26:29 +0900

    [DOOP-18] feat : 위장

---
+++ MyoungJinSeo/src/main/java/com/back/week02/M6.java
@@ -0,0 +1,24 @@
+package com.back.week02;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class M6 {
+    public static void main(String[] args) {
+        String[][] arr = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println(solution(arr));
+    }
+
+    public static int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String[] strings : clothes) {
+            map.put(strings[1], map.getOrDefault(strings[1], 0) + 1);
+        }
+        int answer = 1;
+        for (Integer integer : map.values()) {
+            answer *= integer + 1;
+        }
+
+        return answer - 1;
+    }
+}
