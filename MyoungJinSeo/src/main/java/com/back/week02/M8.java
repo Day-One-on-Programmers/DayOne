4650a56509aadfbf3fce5ed1f38e622411e2f24d
myoungjinseo <sih5673@gmail.com>
2025-08-19 16:31:23 +0900

    [DOOP-18] feat : 로또 번호 검출기

---
+++ MyoungJinSeo/src/main/java/com/back/week02/M8.java
@@ -0,0 +1,17 @@
+package com.back.week02;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class M8 {
+    public static void main(String[] args) {
+        int [] arr = {4,7,32,43,22,19};
+        System.out.println(solution(arr));
+    }
+
+    public static boolean solution(int[] arr) {
+        Set<Integer> set = Arrays.stream(arr).filter(i -> i <= 45 || i >= 1).boxed().collect(Collectors.toSet());
+        return set.size() == arr.length;
+    }
+}
