8d38649462024697d9fffbad3460b1eba2dd6395
경원 <kyungwon.park0407@gmail.com>
2025-08-24 19:59:56 +0900

    [DOOP-17] feat: if문

---
+++ KyungWonPark/src/main/java/com/back/week01/M11.java
@@ -0,0 +1,23 @@
+package com.back.week01;
+
+public class M11 {
+    public int solution(int score) {
+        int grade = 0;
+
+
+        if(score >= 90){
+            grade = 1;
+        }else if(score >= 80) {
+            grade = 2;
+        } else {
+            grade = 3;
+        }
+        // if문 : 90보다 크거나 같으면, 1
+
+        // else if문 : 80보다 크거나 같고 ~ 90보다 작으면, 2
+
+        // else문 : 80보다 작으면, 3
+
+        return grade;
+    }
+}
