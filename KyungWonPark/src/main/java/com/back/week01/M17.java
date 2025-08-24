a073ef4badc2aff2205968f8ff148ac765db48f6
경원 <kyungwon.park0407@gmail.com>
2025-08-24 22:42:27 +0900

    [DOOP-17] feat: 인스턴스

---
+++ KyungWonPark/src/main/java/com/back/week01/M17.java
@@ -0,0 +1,15 @@
+package com.back.week01;
+
+public class M17{
+    public boolean isCar() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Car클래스로부터 객체를 생성해 car 변수명으로 저장하세요.
+
+        M17 car = new M17();
+
+        System.out.println(car.isCar());
+    }
+}
