813514ac1e747717163a4aee03f5597d45a83ac1
경원 <kyungwon.park0407@gmail.com>
2025-08-24 22:17:08 +0900

    [DOOP-17] feat: while문

---
+++ KyungWonPark/src/main/java/com/back/week01/M13.java
@@ -0,0 +1,25 @@
+package com.back.week01;
+
+public class M13 {
+    /**
+     변수 i와 while문을 이용해서 0부터 10까지 중 홀수들의 합을 구해보세요.
+     - while문을 사용해서 반복하고, 홀수를 판단할 때는 if문을 사용합니다.
+     - 결과값은 result 변수에 저장합니다.
+     */
+    public static void main(String[] args) {
+        int i = 0;
+        int result = 0;
+
+        while(i < 10) {
+
+            if(i%2 != 0) {
+                result += i;
+            }
+            i ++;
+        }
+
+        // 0부터 10까지 중 홀수들의 합을 구하세요.
+
+        System.out.println(result);
+    }
+}
